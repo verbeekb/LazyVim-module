8973ee07c4c44ed4752417a09d7c4b2cdd766661
Bram Verbeek <bram.verbeek@vito.be>
2025-05-23 13:06:36 +0200

    feat: gitui init

---
+++ lazyvim/extras/util/gitui.nix
@@ -0,0 +1,24 @@
+self:
+{
+    config,
+    lib,
+    pkgs,
+    ...
+  }:
+  let 
+    inherit (lib.modules) mkIf;
+    inherit (lib.options) mkEnableOption;
+
+    cfg = config.programs.lazyvim;
+  in
+  {
+      options.programs.lazyvim.extras.util.gitui = {
+          enable = mkEnableOption "the util.gitui extra";
+        };
+
+        config = mkIf cfg.extras.util.gitui.enable {
+            extraPackages = [
+              pkgs.gitui
+            ]
+          }
+    }
